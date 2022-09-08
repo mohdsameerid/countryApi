4d4e07a089b4d99429751058eb90c1115c21a459
mohdsameerid <mohdsameeredu@gmail.com>
2022-09-08 12:40:28 +0530

    console all require values of country

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ searchBtn.addEventListener("click", () => {
         return response.json();
     })
     .then((data) => {
-        console.log(data);
+        console.log(data[0]);
+        console.log(data[0].capital[0]);
+        console.log(data[0].flags.png);
+        console.log(data[0].name.common);
+        console.log(data[0].currencies.INR.name);
+        console.log(data[0].currencies.INR.symbol)
+        console.log(data[0].population);
+        console.log(data[0].continents[0]);
+        console.log(Object.values(data[0].languages).toString().split(",").join(", "));
     });
 })
