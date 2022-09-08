afba59f9ae9e246ebee8762ab5d2598da4bc8f7a
mohdsameerid <mohdsameeredu@gmail.com>
2022-09-08 13:55:46 +0530

    added all country deatails

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("country-inp");
 
 searchBtn.addEventListener("click", () => {
-    let countryName = "India";
+    let countryName = countryInp.value;
     let finalUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
     console.log(finalUrl);
     fetch(finalUrl)
@@ -12,12 +12,49 @@ searchBtn.addEventListener("click", () => {
     .then((data) => {
         console.log(data[0]);
         console.log(data[0].capital[0]);
-        console.log(data[0].flags.png);
+        console.log(data[0].flags.svg);
         console.log(data[0].name.common);
-        console.log(data[0].currencies.INR.name);
-        console.log(data[0].currencies.INR.symbol)
+        console.log(Object.keys(data[0].currencies)[0]);
+        console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
         console.log(data[0].population);
         console.log(data[0].continents[0]);
         console.log(Object.values(data[0].languages).toString().split(",").join(", "));
+
+        result.innerHTML = `
+            <img src = "${data[0].flags.svg}" class="flag-img" />
+
+            <h2>${data[0].name.common}</h2>
+            <div class="wrapper">
+                <div class="data-wrapper" >
+                    <h4>Capital:</h4>
+                    <span>${data[0].capital[0]}</span>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="data-wrapper" >
+                    <h4>Continent:</h4>
+                    <span>${data[0].continents[0]}</span>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="data-wrapper" >
+                    <h4>Population:</h4>
+                    <span>${data[0].population}</span>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="data-wrapper" >
+                    <h4>Currency:</h4>
+                    <span>${data[0].currencies[Object.keys(data[0].currencies)].name
+                    } - ${Object.keys(data[0].currencies)[0]}</span>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="data-wrapper" >
+                    <h4>Common Language:</h4>
+                    <span>${Object.values(data[0].languages).toString().split(",").join(", ")}</span>
+                </div>
+            </div>
+        `;
     });
 })
