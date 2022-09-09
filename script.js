5eeb4af8315d0b673b07461c64fc5708308c4bfa
mohdsameerid <mohdsameeredu@gmail.com>
2022-09-09 22:01:20 +0530

    remove consoles

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ let countryInp = document.getElementById("country-inp");
 searchBtn.addEventListener("click", () => {
     let countryName = countryInp.value;
     let finalUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-    console.log(finalUrl);
+    // console.log(finalUrl);
     fetch(finalUrl)
     .then((response) => {
         return response.json();
     })
     .then((data) => {
-        console.log(data[0]);
-        console.log(data[0].capital[0]);
-        console.log(data[0].flags.svg);
-        console.log(data[0].name.common);
-        console.log(Object.keys(data[0].currencies)[0]);
-        console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
-        console.log(data[0].population);
-        console.log(data[0].continents[0]);
-        console.log(Object.values(data[0].languages).toString().split(",").join(", "));
+        // console.log(data[0]);
+        // console.log(data[0].capital[0]);
+        // console.log(data[0].flags.svg);
+        // console.log(data[0].name.common);
+        // console.log(Object.keys(data[0].currencies)[0]);
+        // console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+        // console.log(data[0].population);
+        // console.log(data[0].continents[0]);
+        // console.log(Object.values(data[0].languages).toString().split(",").join(", "));
 
         result.innerHTML = `
             <img src = "${data[0].flags.svg}" class="flag-img" />
